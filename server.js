53d1b3d552f5b3c32e7659aa6d973c9f9599f9bb
iillaa <dr.doodo257@gmail.com>
2026-07-02 02:53:36 +0100

    fix(cors): make Access-Control-Allow-Headers dynamic

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-admin-token, ngrok-skip-browser-warning');
+  const requestedHeaders = req.headers['access-control-request-headers'];
+  res.setHeader('Access-Control-Allow-Headers', requestedHeaders || 'Content-Type, Authorization, x-admin-token, ngrok-skip-browser-warning');
   
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
