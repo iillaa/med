35390c807e446c1ebc7b32bb334b4d53dc8f6f4b
iillaa <dr.doodo257@gmail.com>
2026-07-17 16:33:28 +0100

    feat(vue): scaffold project with Vite, Vue 3, TS, Pinia, Router

---
+++ med-vue/vite.config.ts
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  build: {
+    outDir: '../public',
+  },
+  publicDir: path.resolve(__dirname, '../static'),
+})
