8042b870cff67261ab032ed33ece866aba90ab26
iillaa <dr.doodo257@gmail.com>
2026-07-26 00:54:16 +0100

    fix(android): revert custom WindowCompat hacks and restore clean standard Capacitor BridgeActivity layout

--- android/app/src/main/java/com/drcat/app/MainActivity.java
+++ android/app/src/main/java/com/drcat/app/MainActivity.java
@@ -1,32 +1,5 @@
 package com.drcat.app;
 
-import android.graphics.Color;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Window;
-import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        Window window = getWindow();
-
-        // Normal native layout: WebView fits above system navigation bar
-        WindowCompat.setDecorFitsSystemWindows(window, true);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.setStatusBarColor(Color.parseColor("#090d16"));
-            window.setNavigationBarColor(Color.parseColor("#090d16"));
-        }
-
-        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(window, window.getDecorView());
-        if (controller != null) {
-            controller.setAppearanceLightStatusBars(false);
-            controller.setAppearanceLightNavigationBars(false);
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
