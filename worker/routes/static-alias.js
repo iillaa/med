92c6c76b758d1de2e4d2b27ee4116f56f35fe35f
iillaa <dr.doodo257@gmail.com>
2026-09-02 00:43:17 +0100

    chore(release): bump version to 1.19.0 and stamp build

--- worker/routes/static-alias.js
+++ worker/routes/static-alias.js
@@ -55,7 +55,7 @@ export async function handleStaticAliasRoute(request, env, url) {
   // 6. GET /api/version
   if (url.pathname === '/api/version') {
     return jsonResponse({
-      version: "1.18.0",
+      version: "1.19.0",
       minVersion: "1.0.0"
     });
   }
